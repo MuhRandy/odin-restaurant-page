2240e56db6ed9c5b1e8a1751acf8dd21a0250a1f
MuhRandy <muhammadrandy0027@gmail.com>
2024-09-03 20:52:52 +0800

    feat: add nav logic

--- src/index.js
+++ src/index.js
@@ -67,3 +67,47 @@ restaurantData.contacts.map((contact) => {
 
   contactsContainer.appendChild(content);
 });
+
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const contacts = document.querySelector("#contacts");
+
+const homeNav = document.querySelector("#home-nav");
+const menuNav = document.querySelector("#menu-nav");
+const contactsNav = document.querySelector("#contacts-nav");
+
+const homeNavHandlerClick = () => {
+  homeNav.classList.add("active");
+  menuNav.classList.remove("active");
+  contactsNav.classList.remove("active");
+
+  home.classList.remove("hidden");
+  menu.classList.add("hidden");
+  contacts.classList.add("hidden");
+};
+
+const menuNavHandlerClick = () => {
+  homeNav.classList.remove("active");
+  menuNav.classList.add("active");
+  contactsNav.classList.remove("active");
+
+  home.classList.add("hidden");
+  menu.classList.remove("hidden");
+  contacts.classList.add("hidden");
+};
+
+const contactsNavHandlerClick = () => {
+  homeNav.classList.remove("active");
+  menuNav.classList.remove("active");
+  contactsNav.classList.add("active");
+
+  home.classList.add("hidden");
+  menu.classList.add("hidden");
+  contacts.classList.remove("hidden");
+};
+
+homeNav.addEventListener("click", homeNavHandlerClick);
+menuNav.addEventListener("click", menuNavHandlerClick);
+contactsNav.addEventListener("click", contactsNavHandlerClick);
+
+homeNavHandlerClick();
